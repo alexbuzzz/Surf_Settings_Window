83ed84c54a64565449716f3596f613c3fba2daa6
alexbuzzz <sasha.buzik@gmail.com>
2024-02-08 18:18:48 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Surf_Settings_Window
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ CustomControls/Button1.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Surf_Settings_Window.CustomControls
+{
+    public partial class Button1 : UserControl
+    {
+        public static readonly DependencyProperty ButtonTextProperty =
+            DependencyProperty.Register("ButtonText", typeof(string), typeof(Button1), new PropertyMetadata("Custom Button"));
+
+        public static readonly DependencyProperty IconPathProperty =
+            DependencyProperty.Register("IconPath", typeof(string), typeof(Button1), new PropertyMetadata(""));
+
+        public string ButtonText
+        {
+            get { return (string)GetValue(ButtonTextProperty); }
+            set { SetValue(ButtonTextProperty, value); }
+        }
+
+        public string IconPath
+        {
+            get { return (string)GetValue(IconPathProperty); }
+            set { SetValue(IconPathProperty, value); }
+        }
+
+        public Button1()
+        {
+            InitializeComponent();
+            DataContext = this; // Set UserControl's DataContext to itself for binding to work properly
+        }
+    }
+}
